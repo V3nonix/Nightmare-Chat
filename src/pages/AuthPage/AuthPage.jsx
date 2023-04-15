a79a972d1ce1c91d80175f38db86998c6aa0caf8
V3nonix <liam.grubbs@outlook.com>
2023-04-15 13:29:45 -0700

    Completes temp AuthPage.

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+// Imports stylesheet(s):
+import './AuthPage.css';
+// Imports Components:
+import LoginForm from '../../components/LoginForm/LoginForm';
+import Logo from '../../components/Logo/Logo';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
+export default function AuthPage({ alterUser }) {
+    // Sets state:
+    const [authType, setAuthType] = useState(false);
+    // Event handler functions:
+    function handleClick(evt) {
+        setAuthType(!authType);
+    }
+    // Rendered component:
+    return (
+        <main className='AuthPage'>
+            <div>
+                <Logo />
+                <h3 onClick={handleClick}>{ authType ? 'LOGIN?' : 'SIGN-UP?'}</h3>
+            </div>
+            <div>
+            { authType ?
+                <SignUpForm alterUser={alterUser}/>
+            :
+                <LoginForm alterUser={alterUser}/>
+            }
+            </div>
+        </main>
+    );
+}
